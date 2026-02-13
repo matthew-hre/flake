6ee6f10f46d9ae5995d255534c260eace84c9918
Matthew Hrehirchuk <me@matthew-hre.com>
2026-02-12 19:53:07 -0700

    qs: disable battery for thwomp

--- home/wayland/quickshell/default.nix
+++ home/wayland/quickshell/default.nix
@@ -1,5 +1,12 @@
-{inputs, ...}: {
+{
+  config,
+  inputs,
+  ...
+}: {
   imports = [inputs.quickshell-config.homeManagerModules.default];
 
   # programs.quickshellConfig.devPath = "/home/matthew_hre/.config/quickshell";
+  programs.quickshellConfig = {
+    settings.showBattery = builtins.getEnv "HOSTNAME" != "thwomp";
+  };
 }
